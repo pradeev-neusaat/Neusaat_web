4eee19e4c04f74509882a265ba09609d574f5a96
Pradeev <pradeev.somu@neusaat.com>
2025-04-05 21:07:27 +0530

    Phone nbr update

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -26,7 +26,7 @@ export async function POST(req) {
       from: `"${name}" <${process.env.ZOHO_SMTP_USER}>`,
       to: process.env.ZOHO_SMTP_USER,
       subject: "New Contact Form Submission",
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}\nPhone: ${phone}`,
     };
 
     await transporter.sendMail(mailOptions);
